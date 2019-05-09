aed230765279b764784ff40d63a4674a1fec9744
andrew <voroninandrewi@gmail.com>
2019-05-09 23:33:50 +0300

    Add image format check in face_search.py

--- face_rec/face_search.py
+++ face_rec/face_search.py
@@ -6,22 +6,25 @@ import os
 
 res_dict = Manager().dict()
 
-def count_distance(filename, pth, unknown_face):
-    distances = []
-    
-    known_picture = face_recognition.load_image_file(pth + '/' + filename)
-    known_picture_encoding = face_recognition.face_encodings(known_picture)
 
-    if len(known_picture_encoding) != 0:
-                                                              # cчитаем дистанции от неизвестного лица до каждого лица
-        for known_face_encoding in known_picture_encoding:    # на картинках из профилей
+def count_distance(filename, pth, unknown_face):
+
+    if filename[-3:] == 'jpg':
+        distances = []
+        known_picture = face_recognition.load_image_file(pth + '/' + filename)
+        known_picture_encoding = face_recognition.face_encodings(known_picture)
+
+# cчитаем дистанции от неизвестного лица до каждого лица на картинках из профилей
+        for known_face_encoding in known_picture_encoding:
             distances.append(face_recognition.face_distance([known_face_encoding], unknown_face))
 
-        res_dict[filename] = min(distances)[0]  # ставим в соответствие id пользователя
-                                                # минимальную из найденных дистанций
+# ставим в соответствие id пользователя минимальную из найденных дистанций
+        if len(distances) > 0:
+            res_dict[filename] = min(distances)[0]
+
 
 def get_distances(path_to_dir, unknown_face_enc):
-    p = Pool(1)  #  нужно будет поставить побольше в окончательном варианте
+    p = Pool(8)
     list_of_images = os.listdir(path_to_dir)
     p.map(functools.partial(count_distance, pth=path_to_dir, unknown_face=unknown_face_enc), list_of_images)
 
@@ -31,7 +34,7 @@ def get_distances(path_to_dir, unknown_face_enc):
 
 
 def find_person(path_to_unknown_face='unknown_faces/putin.jpg', dir_known_faces='faces'):
-    
+
     unknown_picture = face_recognition.load_image_file(path_to_unknown_face)
     unknown_picture_encoding = face_recognition.face_encodings(unknown_picture)
 
@@ -41,6 +44,6 @@ def find_person(path_to_unknown_face='unknown_faces/putin.jpg', dir_known_faces=
         print('No faces detected.')
     else:
         unknown_face_encoding = unknown_picture_encoding[0]
-        
+
         users_dict = get_distances(path_to_dir=dir_known_faces, unknown_face_enc=unknown_face_encoding)
         return dict(sorted(users_dict.items(), key=lambda x: x[1]))
